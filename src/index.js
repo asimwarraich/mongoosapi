ce8f6fe29f9f22d375663a8ca376c91e026d6857
asimwarraich <asimwarraich533@gmail.com>
2022-08-04 06:51:25 -0700

    done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
-const cors = require("core");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 //api config
-
+dotenv.config();
+const app = express();
+const port = process.env.port || 9000;
 //midlawyer
 
-// db
+app.use(cors());
+app.use(express.json());
+
+// db configer
+
+//api endpoints
+app.get("/", (req, res) => {
+  res.json({ message: "welcome to the api" });
+});
+//listner
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
